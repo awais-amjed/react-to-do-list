288b119d4f698795ec167cdb829545cb23da75e3
Awais Amjed <awaisamjed171@gmail.com>
2022-03-21 17:26:59 +0500

    Fix Linter Errors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function TodoItem(props) {
-  return <li>{props.todo.title}</li>;
+function TodoItem({ todo }) {
+  TodoItem.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    todo: PropTypes.object.isRequired,
+  };
+
+  return <li>{todo.title}</li>;
 }
 
 export default TodoItem;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    );
-  }
+function TodosList({ todos }) {
+  TodosList.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    todos: PropTypes.array.isRequired,
+  };
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 }
 
 export default TodosList;
